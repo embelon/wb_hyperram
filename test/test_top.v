2a6f924f668b3c293677a12437c2e48453435105
embelon <78412338+embelon@users.noreply.github.com>
2022-02-01 22:11:23 +0100

    Fixed invalid WB address port name. Enabled UserPreload for cocotb testing with S27KL0641 model. Adjusted base addresses in cocotb tests.

--- test/test_top.v
+++ test/test_top.v
@@ -1,19 +1,19 @@
 `default_nettype none
 `timescale 1ps/1ps
 module test_top (
-	input			wb_clk_i,
-	input			wb_rst_i,
+	input		wb_clk_i,
+	input		wb_rst_i,
 	
-	input			wbs_stb_i,
-	input			wbs_cyc_i,
-	input			wbs_we_i,
+	input		wbs_stb_i,
+	input		wbs_cyc_i,
+	input		wbs_we_i,
 	input	[3:0] 	wbs_sel_i,
 	input 	[31:0] 	wbs_dat_i,
-	input	[31:0] 	wbs_addr_i,
-	output			wbs_ack_o,
+	input	[31:0] 	wbs_adr_i,
+	output		wbs_ack_o,
 	output	[31:0] 	wbs_dat_o,	
 
-	input			rst_i
+	input		rst_i
 );
 
     wire [7:0] HB_DQ;
@@ -23,7 +23,12 @@ module test_top (
     wire HB_CLKN;
     wire HB_RSTN;
 
-    s27kl0641 hyperram_mem (
+    s27kl0641 
+    #(
+	.UserPreload(1)
+    )
+    hyperram_mem
+    (
         .DQ7        (HB_DQ[7]),
         .DQ6        (HB_DQ[6]),
         .DQ5        (HB_DQ[5]),
@@ -50,27 +55,27 @@ module test_top (
         .wbs_stb_i      (wbs_stb_i),
         .wbs_cyc_i      (wbs_cyc_i),
         .wbs_we_i       (wbs_we_i),
-        .wbs_sel_i      (wbs_sel_i),
-        .wbs_dat_i      (wbs_dat_i),
-        .wbs_addr_i     (wbs_addr_i),
+        .wbs_sel_i      (wbs_sel_i[3:0]),
+        .wbs_dat_i      (wbs_dat_i[31:0]),
+        .wbs_adr_i     	(wbs_adr_i[31:0]),
         .wbs_ack_o      (wbs_ack_o),
-        .wbs_dat_o      (wbs_dat_o),	
+        .wbs_dat_o      (wbs_dat_o[31:0]),	
 
         .rst_i,
 
-        .hb_rstn_o      (HB_RSTN),	    // #RST
-        .hb_csn_o       (HB_CSN),	    // #CS
+        .hb_rstn_o      (HB_RSTN),	    	// #RST
+        .hb_csn_o       (HB_CSN),	    	// #CS
         .hb_clk_o       (HB_CLK),
-        .hb_clkn_o      (),             // #CLK
+        .hb_clkn_o      (),             	// #CLK
         .hb_rwds_o      (hb_rwds_o),
-        .hb_rwds_oen    (hb_rwds_oen),	// #RWDS_OE
-        .hb_dq_o        (hb_dq_o),
-        .hb_dq_oen      (hb_dq_oen),    // #DQ_OE
+        .hb_rwds_oen    (hb_rwds_oen),		// #RWDS_OE
+        .hb_dq_o        (hb_dq_o[7:0]),
+        .hb_dq_oen      (hb_dq_oen[7:0]),    	// #DQ_OE
         .hb_rwds_i      (HB_RWDS),
-        .hb_dq_i        (HB_DQ)
+        .hb_dq_i        (HB_DQ[7:0])
     );
     
     assign HB_RWDS = hb_rwds_oen ? 1'bZ : hb_rwds_o;
-    assign HB_DQ = hb_dq_oen[0] ? 8'bZZZZZZZZ : hb_dq_o;
+    assign HB_DQ[7:0] = hb_dq_oen[0] ? 8'bZZZZZZZZ : hb_dq_o[7:0];
 
 endmodule
--- test/test_wb_hyperram.py
+++ test/test_wb_hyperram.py
@@ -13,7 +13,7 @@ async def reset(dut):
     dut.wbs_we_i <= 0
     dut.wbs_sel_i <= 0
     dut.wbs_dat_i <= 0
-    dut.wbs_addr_i <= 0
+    dut.wbs_adr_i <= 0
 
     await ClockCycles(dut.wb_clk_i, 5)
     dut.wb_rst_i <= 0
@@ -23,7 +23,7 @@ async def reset(dut):
 @cocotb.test()
 async def test_all(dut):
     clock = Clock(dut.wb_clk_i, 10, units="us")
-    wb_master = WB_Master(dut.wb_clk_i, dut.wbs_cyc_i, dut.wbs_stb_i, dut.wbs_we_i, dut.wbs_addr_i, dut.wbs_sel_i, dut.wbs_dat_i, dut.wbs_dat_o, dut.wbs_ack_o, 100)
+    wb_master = WB_Master(dut.wb_clk_i, dut.wbs_cyc_i, dut.wbs_stb_i, dut.wbs_we_i, dut.wbs_adr_i, dut.wbs_sel_i, dut.wbs_dat_i, dut.wbs_dat_o, dut.wbs_ack_o, 100)
 
     cocotb.fork(clock.start())
 
@@ -33,32 +33,40 @@ async def test_all(dut):
 
     await ClockCycles(dut.wb_clk_i, 5) 
 
+    CSR_BASE_ADDR = 0x30810000
+    REG_BASE_ADDR = 0x30800000
+    RAM_BASE_ADDR = 0x30000000
+
+    REG_LATENCY_OFFSET = 0
+    REG_TCSH_OFFSET = 8
+
+
     # read tcsh CSR
-    read = await wb_master.read(0x30c00008, 0x0f)
+    read = await wb_master.read(CSR_BASE_ADDR + REG_TCSH_OFFSET, 0x0f)
     assert read == 4
 
     await ClockCycles(dut.wb_clk_i, 5) 
 
     # read, write and read back latency CSR
-    await wb_master.read(0x30c00000, 0x0f)
+    await wb_master.read(CSR_BASE_ADDR + REG_LATENCY_OFFSET, 0x0f)
     assert read == 0x36
-    await wb_master.write(0x30c00000, 0x0f, 0x14)
-    await wb_master.read(0x30c00000, 0x0f)
+    await wb_master.write(CSR_BASE_ADDR + REG_LATENCY_OFFSET, 0x0f, 0x14)
+    await wb_master.read(CSR_BASE_ADDR + REG_LATENCY_OFFSET, 0x0f)
     assert read == 0x14
-    await wb_master.write(0x30c00000, 0x0f, 0x36)
-    await wb_master.read(0x30c00000, 0x0f)
+    await wb_master.write(CSR_BASE_ADDR + REG_LATENCY_OFFSET, 0x0f, 0x36)
+    await wb_master.read(CSR_BASE_ADDR + REG_LATENCY_OFFSET, 0x0f)
     assert read == 0x36
 
     await ClockCycles(dut.wb_clk_i, 5) 
 
     # read register space
-    read = await wb_master.read(0x30801000, 0x0f)
+    read = await wb_master.read(REG_BASE_ADDR + 0x1000, 0x0f)
     assert read != 0
 
     await ClockCycles(dut.wb_clk_i, 5) 
 
     # HyperRAM should be filled according to s27kl0641/model/s27kl0641.mem file
-    read = await wb_master.read(0x30000006, 0x0f)
+    read = await wb_master.read(RAM_BASE_ADDR + 6, 0x0f)
     assert read != 0
     assert read == 0x00040005
 
@@ -66,8 +74,8 @@ async def test_all(dut):
 
     # write and read some value to memory space
     value = 0xa5a54224
-    await wb_master.write(0x30001000, 0x0f, value)
-    read = await wb_master.read(0x30001000, 0x0f)
+    await wb_master.write(RAM_BASE_ADDR + 0x1000, 0x0f, value)
+    read = await wb_master.read(RAM_BASE_ADDR + 0x1000, 0x0f)
     assert read == value
 
 
